20f3dee72f0b180b32ec15bbadedf405d4592f4c
PJ Go <pjgo@tweeklabs.com>
2018-01-23 10:28:36 +0800

    update

--- src/Transformers/UserTransformer.php
+++ src/Transformers/UserTransformer.php
@@ -5,36 +5,27 @@ namespace Sunnydevbox\Boticajohn\Transformers;
 // use Dingo\Api\Http\Request;
 // use Dingo\Api\Transformer\Binding;
 // use Dingo\Api\Contract\Transformer\Adapter;
-
 use Sunnydevbox\TWUser\Transformers\RoleTransformer;
 use Sunnydevbox\TWUser\Transformers\UserTransformer as ExtendUsertransformer;
 
 class UserTransformer extends ExtendUsertransformer
 {
-     /**
-     * Include user profile data by default
-     */
-    protected $defaultIncludes =   [ 'roles'];
-
-    protected $mode = 'complete'; // 'basic', 'complete'
-
-    public function setMode(String $mode)
-    {
-        $this->mode = $mode;
-    }
 
     public function transform($obj)
     {
+
         if ($this->mode == 'basic') {
             $data = [
                 'id' => $obj->id,
+                'first_name' => $obj->getMeta('first_name'),
+                'last_name' => $obj->getMeta('last_name'),
             ];
+            
         } else if ($this->mode == 'complete'){
 
             $data = [
                 'id'            => (int) $obj->id,
                 'email'         => $obj->email,
-                'events'        => $obj->events,
 
                 'date_joined'       => date('Y-m-d H:i:s', strtotime($obj->created_at)),
                 'last_update'   => date('Y-m-d H:i:s', strtotime($obj->updated_at)),
@@ -43,7 +34,7 @@ class UserTransformer extends ExtendUsertransformer
                 'is_verified'   => ($obj->is_verified) ? strtotime($obj->is_verified) : false,
             ];
             
-            if (method_exists($obj,'getAllMeta')) {
+            if (method_exists(config('tw-user.api.users.model'),'getAllMeta')) {
                 
                 $metas = collect($obj->getAllMeta());
                 
@@ -56,26 +47,4 @@ class UserTransformer extends ExtendUsertransformer
         return $data;
     }
 
-    public function includeRoles($model)
-    {
-        if ($model->roles) {
-            return $this->collection($model->roles, new RoleTransformer());
-        } else {
-            return null;
-        }
-    }
-
-    public function includeMeta($model) 
-    {
-        $data = collect([]);
-        
-        if (method_exists($model,'getAllMeta')) {
-            foreach ($model->getAllMeta() as $k => $v) {
-
-                //$data->put($k, $v);
-            }
-        }
-        
-        return $data;
-    }
 }
